823eaaa2c87ef7348b2421906c73083bcae8cba5
Pintro <reiandika10@gmail.com>
2022-06-27 14:43:26 +0700

    Initial Commit

---
+++ Happy Nice Day/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Happy Nice Day
+//
+//  Created by Pintro on 6/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
